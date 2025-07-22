0eb5851db021b1b907d4f0f2ffb5207c2c2b44e6
shyytsm <139909980+shyytsm@users.noreply.github.com>
2025-07-22 19:06:00 +0200

    Update clean-my-pc.sh

--- clean-my-pc.sh
+++ clean-my-pc.sh
@@ -2,14 +2,14 @@
 
 echo " Nettoyage du système..."
 
-# Nettoyage du cache utilisateur
+# note Nettoyage du cache utilisateur
 rm -rf ~/.cache/*
 
-# Nettoyage des paquets inutilisés
+# note Nettoyage des paquets inutilisés
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 
-# Nettoyage des journaux système
+# note Nettoyage des journaux système
 sudo journalctl --vacuum-time=7d
 
 echo "Nettoyage terminé."
